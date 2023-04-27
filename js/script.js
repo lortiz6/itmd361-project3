0dca05a1dca5ffc86077cc18172f88f14ed03763
Liliana Ortiz <lilianadcortiz@gmail.com>
2023-04-26 19:58:57 -0500

    adding JavaScript code to script.js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function initMap(){
+  var getMap = document.getElementById('map');
+  var myLocation = new google.maps.LatLng(41.83521262082427, -87.62550989609518);
+  var setMap = {
+    center: myLocation,
+    zoom: 15
+  }
+  var project3map = new google.maps.Map(getMap, setMap);
+  var marker = new google.maps.Marker({
+    position: myLocation,
+    map: project3map,
+    animation: google.maps.Animation.BOUNCE,
+    icon: 'media/mymelody.png'
+  })
+  google.maps.event.addListener(marker, 'click', function(){alert("YOU HAVE ARRIVED!");});
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
